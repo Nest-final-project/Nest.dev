ba459a293b6c8e12c4ac1b1c2b89ec11e0e194a9
suj9810 <suj9810@naver.com>
2025-05-30 11:36:30 +0900

    repository 생성

---
+++ src/main/java/caffeine/nest_dev/Application.java
@@ -0,0 +1,13 @@
+package caffeine.nest_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
