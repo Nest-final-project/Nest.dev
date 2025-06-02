b851d6fbc90762db23c3f6bf38ce0f9a9f098fb8
chohyeokjun <chj3731@naver.com>
2025-06-02 14:47:53 +0900

    feat: ErrorCode 구현

--- src/main/java/caffeine/nest_dev/common/enums/ErrorCode.java
+++ src/main/java/caffeine/nest_dev/common/enums/ErrorCode.java
@@ -6,7 +6,9 @@ public enum ErrorCode implements BaseCode {
     // Auth
     UNAUTHORIZED_ROLE(HttpStatus.FORBIDDEN, "권한이 없는 유저입니다."),
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증되지 않은 사용자입니다."),
-    NO_PERMISSION(HttpStatus.FORBIDDEN, "접근 권한이 없습니다.");
+    NO_PERMISSION(HttpStatus.FORBIDDEN, "접근 권한이 없습니다."),
+    IS_BLACKLISTED(HttpStatus.UNAUTHORIZED, "로그아웃 된 토큰입니다.");
+
 
 
     private final HttpStatus httpStatus;
