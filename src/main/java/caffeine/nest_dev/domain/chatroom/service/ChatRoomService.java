7f06e6b00f9545652cd43821b231d6ef6e7c12f6
gganb <gksql0801@naver.com>
2025-06-02 20:47:16 +0900

    feat : 채팅방 목록 조회 기능 구현

--- src/main/java/caffeine/nest_dev/domain/chatroom/service/ChatRoomService.java
+++ src/main/java/caffeine/nest_dev/domain/chatroom/service/ChatRoomService.java
@@ -7,6 +7,7 @@ import caffeine.nest_dev.domain.chatroom.repository.ChatRoomRepository;
 import caffeine.nest_dev.domain.reservation.entity.Reservation;
 import caffeine.nest_dev.domain.reservation.repository.ReservationRepository;
 import caffeine.nest_dev.domain.user.entity.User;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,6 +24,7 @@ public class ChatRoomService {
     @Transactional
     public ChatRoomResponseDto createChatRooms(CreateChatRoomRequestDto requestDto) {
 
+        // TODO : 존재하는 채팅방 일 경우 기존의 채팅방을 반환하는 로직 추가
         // 예약이 유효한지 확인
         Reservation reservation = reservationRepository.findById(requestDto.getReservationId()).orElseThrow(
                 () -> new IllegalArgumentException("예약이 존재하지 않습니다.")
@@ -40,5 +42,13 @@ public class ChatRoomService {
 
         ChatRoom savedChatRoom = chatRoomRepository.save(chatRoom);
         return ChatRoomResponseDto.of(savedChatRoom);
+    }
+
+    @Transactional(readOnly = true)
+    public List<ChatRoomResponseDto> findAllChatRooms(Long userId) {
+        // 본인의 id를 가지고있는 채팅방 목록을 가져와야함      ????
+        // 그런데 만약에 자기가 멘티인데 멘토의 목록을 가져올수도 있잖아
+        // 어 그런데 우리 멘토멘티 전환 안되면 ?
+        return null;
     }
 }
