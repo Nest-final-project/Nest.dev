b00fe471fc14d01bae06e11ff114b5460cd3eafd
gganb <gksql0801@naver.com>
2025-06-03 17:41:57 +0900

    feat : 중복 생성 방지 처리

--- src/main/java/caffeine/nest_dev/domain/chatroom/repository/ChatRoomRepository.java
+++ src/main/java/caffeine/nest_dev/domain/chatroom/repository/ChatRoomRepository.java
@@ -1,9 +1,11 @@
 package caffeine.nest_dev.domain.chatroom.repository;
 
 import caffeine.nest_dev.domain.chatroom.entity.ChatRoom;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
 
+    Optional<ChatRoom> findByReservationId(Long reservationId);
 }
--- src/main/java/caffeine/nest_dev/domain/chatroom/service/ChatRoomService.java
+++ src/main/java/caffeine/nest_dev/domain/chatroom/service/ChatRoomService.java
@@ -8,6 +8,7 @@ import caffeine.nest_dev.domain.reservation.entity.Reservation;
 import caffeine.nest_dev.domain.reservation.repository.ReservationRepository;
 import caffeine.nest_dev.domain.user.entity.User;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,11 +25,17 @@ public class ChatRoomService {
     @Transactional
     public ChatRoomResponseDto createChatRooms(CreateChatRoomRequestDto requestDto) {
 
-        // TODO : 존재하는 채팅방 일 경우 기존의 채팅방을 반환하는 로직 추가
         // 예약이 유효한지 확인
         Reservation reservation = reservationRepository.findById(requestDto.getReservationId()).orElseThrow(
                 () -> new IllegalArgumentException("예약이 존재하지 않습니다.")
         );
+
+        // 채팅방이 이미 존재하는 경우 기존의 채팅방을 반환
+        Optional<ChatRoom> existChatRoom = chatRoomRepository.findByReservationId(reservation.getId());
+        if (existChatRoom.isPresent()) {
+            return ChatRoomResponseDto.of(existChatRoom.get());
+        }
+        
         // 멘토, 멘티 정보 추출
         User mentor = reservation.getMentor();
         User mentee = reservation.getMentee();
