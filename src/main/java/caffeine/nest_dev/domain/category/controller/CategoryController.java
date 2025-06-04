599b9490eb8d26c3f398fabe924c5ebb25ff4076
younseung Lee <lys5654@cu.ac.kr>
2025-06-04 20:55:10 +0900

    fix: dto에 변환메서드 빌더

--- src/main/java/caffeine/nest_dev/domain/category/controller/CategoryController.java
+++ src/main/java/caffeine/nest_dev/domain/category/controller/CategoryController.java
@@ -7,11 +7,13 @@ import caffeine.nest_dev.domain.admin.dto.response.AdminMentorCareerResponseDto;
 import caffeine.nest_dev.domain.category.dto.request.CategoryRequestDto;
 import caffeine.nest_dev.domain.category.dto.response.CategoryResponseDto;
 import caffeine.nest_dev.domain.category.service.CategoryService;
+import caffeine.nest_dev.domain.user.entity.UserDetailsImpl;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -25,10 +27,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class CategoryController {
+
     private final CategoryService categoryService;
 
     @PostMapping("/admin/categories")
-    public ResponseEntity<CommonResponse<CategoryResponseDto>> createCategory(@RequestBody @Valid CategoryRequestDto categoryRequestDto) {
+    public ResponseEntity<CommonResponse<CategoryResponseDto>> createCategory(
+            @RequestBody @Valid CategoryRequestDto categoryRequestDto) {
         CategoryResponseDto responseDto = categoryService.creatCategory(categoryRequestDto);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(CommonResponse.of(SuccessCode.SUCCESS_CATEGORY_CREATED, responseDto));
@@ -46,8 +50,9 @@ public class CategoryController {
     public ResponseEntity<CommonResponse<CategoryResponseDto>> updateCategory(
             @PathVariable Long categoryId,
             @RequestBody @Valid CategoryRequestDto categoryRequestDto
-    ){
-        CategoryResponseDto responseDto = categoryService.updateCategory(categoryId, categoryRequestDto);
+    ) {
+        CategoryResponseDto responseDto = categoryService.updateCategory(categoryId,
+                categoryRequestDto);
 
         return ResponseEntity.status(HttpStatus.OK)
                 .body(CommonResponse.of(SuccessCode.SUCCESS_CATEGORY_UPDATED, responseDto));
