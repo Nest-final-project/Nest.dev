e8e0359927613f778a672dce9287fea306232fbe
hyejeongjin <hyejeongjin.dev@gmail.com>
2025-06-04 20:03:45 +0900

    fix: 불필요한 코드 삭제.

--- src/main/java/caffeine/nest_dev/domain/reservation/repository/ReservationRepository.java
+++ src/main/java/caffeine/nest_dev/domain/reservation/repository/ReservationRepository.java
@@ -1,7 +1,6 @@
 package caffeine.nest_dev.domain.reservation.repository;
 
 import caffeine.nest_dev.domain.reservation.entity.Reservation;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
