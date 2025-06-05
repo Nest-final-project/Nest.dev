019f35628d8db9b310eb9fa8f617dfe6928b4613
hyejeongjin <hyejeongjin.dev@gmail.com>
2025-06-05 17:41:24 +0900

    fix: 불필요한 코드 삭제

--- src/main/java/caffeine/nest_dev/domain/review/dto/response/ReviewResponseDto.java
+++ src/main/java/caffeine/nest_dev/domain/review/dto/response/ReviewResponseDto.java
@@ -1,11 +1,9 @@
 package caffeine.nest_dev.domain.review.dto.response;
 
 import caffeine.nest_dev.domain.review.entity.Review;
-import caffeine.nest_dev.domain.user.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import org.springframework.data.domain.Page;
 
 @Getter
 @Builder
