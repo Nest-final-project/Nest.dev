c93d12df60995c9977f9e977f77347f61b2c242c
chohyeokjun <chj3731@naver.com>
2025-06-05 09:24:46 +0900

    feat: 경력 생성 기능 구현

--- src/main/java/caffeine/nest_dev/domain/career/dto/request/CareerRequestDto.java
+++ src/main/java/caffeine/nest_dev/domain/career/dto/request/CareerRequestDto.java
@@ -1,7 +1,29 @@
 package caffeine.nest_dev.domain.career.dto.request;
 
+import caffeine.nest_dev.domain.career.entity.Career;
+import caffeine.nest_dev.domain.career.enums.CareerStatus;
+import caffeine.nest_dev.domain.profile.entity.Profile;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class CareerRequestDto {
+
+    private String company;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private List<String> certificates;
+
+    public Career toEntity(CareerRequestDto dto, Profile profile) {
+        return Career.builder()
+                .company(dto.company)
+                .profile(profile)
+                .startAt(dto.startAt)
+                .endAt(dto.endAt)
+                .careerStatus(CareerStatus.UNAUTHORIZED)
+                .build();
+    }
 }
