048fc3330b97d9ae0568b547ac11c996a0a081dd
gganb <gksql0801@naver.com>
2025-06-05 15:09:22 +0900

    fix : 구독 메시지 접두사 추가 - 아직 인증절차가 없기에 "topic" 접두사를 추가해 해당 uri로 보내면 "/topic/message"를 구독한 클라이언트 모두에게 메시지 전달하도록 설정함

--- src/main/java/caffeine/nest_dev/common/config/WebSocketConfig.java
+++ src/main/java/caffeine/nest_dev/common/config/WebSocketConfig.java
@@ -12,14 +12,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws") // handshake 를 위해 연결하는 endpoint
+        registry.addEndpoint("/ws-nest") // handshake 를 위해 연결하는 endpoint
                 .setAllowedOriginPatterns("*")  // cors 설정 (허용할 origin 지정)
                 .withSockJS();  // 웹소켓을 지원하지 않는 브라우저도 사용할 수 있는 대체 옵션 지정
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/queue");    // 메시지 브로커 활성화, 구독 메시지 접두사 설정 -> 메시지 받을 때
+        registry.enableSimpleBroker("/queue", "/topic");    // 메시지 브로커 활성화, 구독 메시지 접두사 설정 -> 메시지 받을 때
         // 클라이언트에서 발생한 메시지 중 Destination이 해당 경로로 시작하는 메시지를 메시지 브로커에서 처리하도록 함
         registry.setApplicationDestinationPrefixes("/app"); // 메시지를 보낼 때
         registry.setUserDestinationPrefix("/user"); // 본인에게 오는 메시지만 받도록
