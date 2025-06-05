f285eae878981c747c8e895c0d60f9f0495d719c
hyejeongjin <hyejeongjin.dev@gmail.com>
2025-06-05 11:22:01 +0900

    fix: 불필요한 코드 삭제.

--- src/main/java/caffeine/nest_dev/domain/complaint/service/ComplaintService.java
+++ src/main/java/caffeine/nest_dev/domain/complaint/service/ComplaintService.java
@@ -11,7 +11,6 @@ import caffeine.nest_dev.domain.reservation.entity.Reservation;
 import caffeine.nest_dev.domain.reservation.repository.ReservationRepository;
 import caffeine.nest_dev.domain.user.entity.User;
 import caffeine.nest_dev.domain.user.repository.UserRepository;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
