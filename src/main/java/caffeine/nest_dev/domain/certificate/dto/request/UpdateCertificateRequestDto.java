f69cebd70e7fcef7e420b6a424340d59bd873185
chohyeokjun <chj3731@naver.com>
2025-06-05 17:04:29 +0900

    refactor: 기본생성자 생성 어노테이션 추가 및 @Transactional 추가

--- src/main/java/caffeine/nest_dev/domain/certificate/dto/request/UpdateCertificateRequestDto.java
+++ src/main/java/caffeine/nest_dev/domain/certificate/dto/request/UpdateCertificateRequestDto.java
@@ -3,9 +3,11 @@ package caffeine.nest_dev.domain.certificate.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class UpdateCertificateRequestDto {
 
     private String fileUrl;
