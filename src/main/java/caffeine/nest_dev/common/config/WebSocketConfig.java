a8328ce62ab83d966d2943c2527a75f2a250fa5c
gganb <gksql0801@naver.com>
2025-06-07 21:45:03 +0900

    docs : 주석 설정

--- src/main/java/caffeine/nest_dev/common/config/WebSocketConfig.java
+++ src/main/java/caffeine/nest_dev/common/config/WebSocketConfig.java
@@ -8,26 +8,41 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+/**
+ * 웹소켓 메시지 브로커 설정 클래스 STOMP 프로토콜을 기반으로 하는 웹 소켓 통신 설정
+ */
 @Configuration
 @EnableWebSocketMessageBroker
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    /**
+     * 웹소켓 연결 시 인증을 위한 HandShake 인터셉터
+     */
     private final WebSocketAuthInterceptor webSocketAuthInterceptor;
 
+    /**
+     * STOMP 엔드포인트 등록 해당 엔드포인트로 웹소켓 연결
+     *
+     * @param registry STOMP 엔드포인트 등록용 객체
+     */
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-nest") // handshake 를 위해 연결하는 endpoint
-                .addInterceptors(webSocketAuthInterceptor) // socket 연결 전 설정
+        registry.addEndpoint("/ws-nest") // handshake 를 위해(최초 연결 시) 연결하는 endpoint
+                .addInterceptors(webSocketAuthInterceptor) // socket 연결 전 인증, 검증 로직 수행
                 .setAllowedOriginPatterns("*")  // cors 설정 (허용할 origin 지정)
                 .withSockJS();  // 웹소켓을 지원하지 않는 브라우저도 사용할 수 있는 대체 옵션 지정
     }
 
+    /**
+     * 서버와 클라이언트 간 메시지 송수신 경로 정의
+     *
+     * @param registry 메시지 브로커 등록 객체
+     */
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/queue");    // 메시지 브로커 활성화, 구독 메시지 접두사 설정 -> 메시지 받을 때
-        // 클라이언트에서 발생한 메시지 중 Destination이 해당 경로로 시작하는 메시지를 메시지 브로커에서 처리하도록 함
-        registry.setApplicationDestinationPrefixes("/app"); // 메시지를 보낼 때
+        registry.enableSimpleBroker("/queue"); // 구독 경로 (ex) /user/queue/message
+        registry.setApplicationDestinationPrefixes("/app"); // 메시지를 보낼 경로
         registry.setUserDestinationPrefix("/user"); // 본인에게 오는 메시지만 받도록
     }
 
