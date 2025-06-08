bb329ca1c4f08f090391fa14a9b44a7e0eb45df7
gganb <gksql0801@naver.com>
2025-06-08 23:11:56 +0900

    fix : dto 필드 수정 - 예약이 생성된 후, 채팅방 생성, 이후 채팅방 id를 이용해 채팅을 시작하므로 예약 id를 받지 않도록 수정 - 사용자 인증을 추가해 senderId 제거 - 서버에서 받는사람 id를 추적할 수 있으므로 receiverId 제거

--- src/main/java/caffeine/nest_dev/domain/message/dto/request/MessageRequestDto.java
+++ src/main/java/caffeine/nest_dev/domain/message/dto/request/MessageRequestDto.java
@@ -2,7 +2,6 @@ package caffeine.nest_dev.domain.message.dto.request;
 
 import caffeine.nest_dev.domain.chatroom.entity.ChatRoom;
 import caffeine.nest_dev.domain.message.entity.Message;
-import caffeine.nest_dev.domain.reservation.entity.Reservation;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,16 +11,11 @@ import lombok.Setter;
 @NoArgsConstructor
 public class MessageRequestDto {
 
-    private Long roomId;
-    private Long reservationId;
-    private Long senderId;
-    private Long receiverId;
     private String content;
 
-    public Message toEntity(ChatRoom chatRoom, Reservation reservation) {
+    public Message toEntity(ChatRoom chatRoom) {
         return Message.builder()
-                .chatRoom(chatRoom)
-                .reservation(reservation)
+                .reservation(chatRoom.getReservation())
                 .mentor(chatRoom.getMentor())
                 .mentee(chatRoom.getMentee())
                 .content(this.getContent())
