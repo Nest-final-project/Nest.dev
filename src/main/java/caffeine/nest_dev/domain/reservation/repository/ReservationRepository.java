393ae868a1f8d55b51dba68dfe67dafb8fa13df1
hyejeongjin <hyejeongjin.dev@gmail.com>
2025-06-09 17:30:13 +0900

    feat: 예약 CRUD 기능 생성.

--- src/main/java/caffeine/nest_dev/domain/reservation/repository/ReservationRepository.java
+++ src/main/java/caffeine/nest_dev/domain/reservation/repository/ReservationRepository.java
@@ -1,8 +1,11 @@
 package caffeine.nest_dev.domain.reservation.repository;
 
 import caffeine.nest_dev.domain.reservation.entity.Reservation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
+    Page<Reservation> findByMentorIdOrMenteeId(Long mentorId, Long menteeId, Pageable pageable);
 }
