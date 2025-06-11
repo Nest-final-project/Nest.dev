85cfd43bb910f4dcf1f6586f8e452b87ae8280f0
gganb <gksql0801@naver.com>
2025-06-11 19:33:57 +0900

    feat : taskscheduler용 config 생성

---
+++ src/main/java/caffeine/nest_dev/common/config/SchedulerConfig.java
@@ -0,0 +1,16 @@
+package caffeine.nest_dev.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@Configuration
+public class SchedulerConfig {
+
+    public ThreadPoolTaskScheduler chatRoomScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(5);   // 스레드 풀의 스레드 수
+        scheduler.setThreadNamePrefix("chatRoom-scheduler-"); // prefix
+        scheduler.setWaitForTasksToCompleteOnShutdown(true); // 애플리케이션이 종료될 때, 실행중인 스케줄러 작업이 완료될 때까지 기다림
+        return scheduler;
+    }
+}
