4656528cb7a1c24f25ab9fe8ec3706599ed8d9a8
gganb <gksql0801@naver.com>
2025-06-11 19:31:14 +0900

    feat : 채팅방 생성 상태를 저장하기 위한 엔티티 - 서버가 다운되면 예약되었지만 실행되지 못한 작업들이 사라지기에 백업을 위해 저장해야함

---
+++ src/main/java/caffeine/nest_dev/domain/chatroom/scheduler/entity/ChatRoomSchedule.java
@@ -0,0 +1,43 @@
+package caffeine.nest_dev.domain.chatroom.scheduler.entity;
+
+import caffeine.nest_dev.domain.chatroom.scheduler.enums.ScheduleStatus;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 채팅방 예약 상태 저장을 위한 Entity
+ */
+@Entity
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChatRoomSchedule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 예약 id
+    private Long reservationId;
+
+    // 생성 예정 시간
+    private LocalDateTime scheduledTime;
+
+    // 채팅방 예약 상태
+    @Enumerated(EnumType.STRING)
+    private ScheduleStatus scheduleStatus;
+
+    public void updateStatus() {
+        this.scheduleStatus = ScheduleStatus.COMPLETE;
+    }
+}
