e8728b070610acafc5f688566374a05f370c803f
gganb <gksql0801@naver.com>
2025-06-15 23:10:23 +0900

    feat : 작업 등록 - 인증방식 수정 필요

--- src/main/java/caffeine/nest_dev/domain/notification/controller/NotificationController.java
+++ src/main/java/caffeine/nest_dev/domain/notification/controller/NotificationController.java
@@ -1,13 +1,13 @@
 package caffeine.nest_dev.domain.notification.controller;
 
+import caffeine.nest_dev.common.config.JwtUtil;
 import caffeine.nest_dev.domain.notification.service.NotificationService;
-import caffeine.nest_dev.domain.user.entity.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
@@ -17,11 +17,12 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 public class NotificationController {
 
     private final NotificationService notificationService;
+    private final JwtUtil jwtUtil;
 
     @GetMapping(value = "/notifications/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public SseEmitter subscribe(@AuthenticationPrincipal UserDetailsImpl userDetails,
+    public SseEmitter subscribe(@RequestParam("token") String token,
             @RequestHeader(value = "Last-Event-ID", required = false, defaultValue = "") String lastEventId) {
-        Long userId = userDetails.getId();
+        Long userId = jwtUtil.getUserIdFromToken(token);
         return notificationService.subscribe(userId, lastEventId);
     }
 }
