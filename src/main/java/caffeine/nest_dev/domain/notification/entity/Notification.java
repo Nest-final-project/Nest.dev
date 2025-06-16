79ba8142875b671b904e27799e0a8e8a65f94fed
gganb <gksql0801@naver.com>
2025-06-16 11:27:33 +0900

    fix : joincolumn 추가, 생성일 자동 설정 어노테이션 추가

--- src/main/java/caffeine/nest_dev/domain/notification/entity/Notification.java
+++ src/main/java/caffeine/nest_dev/domain/notification/entity/Notification.java
@@ -6,6 +6,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
@@ -13,6 +14,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
 
 /**
  * 사용자에게 전송되는 알림 정보 저장
@@ -29,10 +31,12 @@ public class Notification {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "receiver_id", nullable = false)
     private User receiver;  // 알림 수신자
 
     private String content;
 
+    @CreatedDate
     private LocalDateTime createdAt;
 
 }
