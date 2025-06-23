1c80de3670e3919b8f81b3f6df4592da32adf438
chohyeokjun <chj3731@naver.com>
2025-06-23 12:39:37 +0900

    feat: 로그인된 유저의 모든 경력 목록 조회 기능 추가

--- src/main/java/caffeine/nest_dev/domain/career/repository/CareerRepository.java
+++ src/main/java/caffeine/nest_dev/domain/career/repository/CareerRepository.java
@@ -1,11 +1,13 @@
 package caffeine.nest_dev.domain.career.repository;
 
 import caffeine.nest_dev.domain.career.entity.Career;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -17,4 +19,8 @@ public interface CareerRepository extends JpaRepository<Career, Long> {
     Optional<Career> findByIdAndProfileId(Long careerId, Long profileId);
 
     Page<Career> findAllByProfileId(Long profileId, Pageable pageable);
+
+    @Query("SELECT c FROM Career c WHERE c.profile.id IN :profileIds")
+    Page<Career> findAllByProfileIdsIn(@Param("profileIds") List<Long> profileIds,
+            Pageable pageable);
 }
