2315164cd9d5d7a4950dc0f687e09b2dac7c8567
chohyeokjun <chj3731@naver.com>
2025-06-23 20:44:49 +0900

    refactor: 결제 내역 조회 추가

--- src/main/java/caffeine/nest_dev/domain/profile/repository/ProfileRepository.java
+++ src/main/java/caffeine/nest_dev/domain/profile/repository/ProfileRepository.java
@@ -2,15 +2,13 @@ package caffeine.nest_dev.domain.profile.repository;
 
 import caffeine.nest_dev.domain.profile.entity.Profile;
 import java.util.List;
-import java.util.Optional;
-import org.antlr.v4.runtime.atn.SemanticContext.OR;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 public interface ProfileRepository extends JpaRepository<Profile, Long>, ProfileRepositoryQuery {
 
     Page<Profile> findByUserId(Long userId, Pageable pageable);
+
+    List<Profile> findByUserIdAndIsDeletedFalse(Long userId);
 }
