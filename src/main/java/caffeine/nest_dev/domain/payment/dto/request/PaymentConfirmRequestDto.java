2b8edb3d6cfd926ea9a30926fb168ed4999733d7
suj9810 <suj9810@naver.com>
2025-06-24 12:00:20 +0900

    fix: 유저 쿠폰 사용 시 결제 준비 과정에 적용, 할인 전 원가 금액 entity에 저장

--- src/main/java/caffeine/nest_dev/domain/payment/dto/request/PaymentConfirmRequestDto.java
+++ src/main/java/caffeine/nest_dev/domain/payment/dto/request/PaymentConfirmRequestDto.java
@@ -3,15 +3,18 @@ package caffeine.nest_dev.domain.payment.dto.request;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class PaymentConfirmRequestDto {
 
     private String paymentKey;      // 고유 결제 key 값
     private String orderId;         // 주문 번호
     private Integer amount;         // 결제 금액
+    private Long reservationId;     // 예약 ID (프론트엔드에서 전달)
 }
