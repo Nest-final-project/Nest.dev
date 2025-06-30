6b959b7c350ecd87b85c3d710c8e83ed49b008c0
hyejeongjin <hyejeongjin.dev@gmail.com>
2025-06-30 22:51:55 +0900

    feat: admin, user 각각 답변 조회 메서드 추가.

--- src/main/java/caffeine/nest_dev/domain/complaint/repository/AnswerRepository.java
+++ src/main/java/caffeine/nest_dev/domain/complaint/repository/AnswerRepository.java
@@ -1,8 +1,11 @@
 package caffeine.nest_dev.domain.complaint.repository;
 
 import caffeine.nest_dev.domain.complaint.entity.Answer;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
     boolean existsByComplaintId(Long complaintId);
+
+    Optional<Answer> findByComplaint_Id(Long complaintId);
 }
