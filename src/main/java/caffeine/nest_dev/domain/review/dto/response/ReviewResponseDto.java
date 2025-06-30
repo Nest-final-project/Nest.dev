b1d79ed3e4dde38dd1b51f1e6a9bc5d548eac4e1
suj9810 <suj9810@naver.com>
2025-06-30 22:11:14 +0900

    refactor: response 생성 날짜 추가

--- src/main/java/caffeine/nest_dev/domain/review/dto/response/ReviewResponseDto.java
+++ src/main/java/caffeine/nest_dev/domain/review/dto/response/ReviewResponseDto.java
@@ -2,6 +2,7 @@ package caffeine.nest_dev.domain.review.dto.response;
 
 import caffeine.nest_dev.domain.review.entity.Review;
 import caffeine.nest_dev.domain.review.enums.ReviewStatus;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -23,6 +24,8 @@ public class ReviewResponseDto {
 
     private ReviewStatus reviewStatus;
 
+    private LocalDateTime createdAt;
+
     public static ReviewResponseDto of(Review review){
         return ReviewResponseDto.builder()
                 .id(review.getId())
@@ -30,6 +33,7 @@ public class ReviewResponseDto {
                 .mentor(review.getMentor().getId())
                 .mentee(review.getMentee().getId())
                 .content(review.getContent())
+                .createdAt(review.getCreatedAt())
                 .reviewStatus(review.getReviewStatus())
                 .build();
     }
